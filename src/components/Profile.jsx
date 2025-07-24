c15faa85f9c3a6a8a2f0686754b7c8975db2df4b
kellyman1717 <akuntumbal9419@gmail.com>
2025-07-24 10:54:21 +0700

    menambahkan ornamen pada profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,11 @@
 import { invitationData } from '../data/invitationData.js';
+import profileBg from '../assets/images/profile-bg.png';
+import bismillah from '../assets/images/bismillah.png';
+import flower from '../assets/images/blue_rose_flower.png';
+import flower2 from '../assets/images/blue_floral_bouquet 2.png';
+import flower3 from '../assets/images/blue_floral_bouquet 1.png';
+import fern from '../assets/images/fern-1.png';
+import fern2 from '../assets/images/fern-2.png';
 
 const ProfileCard = ({ name, fullName, father, mother, photo, isBride }) => (
   <div className="flex flex-col items-center gap-4">
@@ -18,9 +25,30 @@ const ProfileCard = ({ name, fullName, father, mother, photo, isBride }) => (
 
 const Profile = () => {
   return (
-    <div className="text-center py-16 px-6 bg-custom-blue-light">
-      <p className="font-sans mb-4">Assalamualaikum Warahmatullahi Wabarakatuh</p>
-      <p className="max-w-3xl mx-auto mb-12">
+    <div className="text-center py-16 px-6 bg-custom-blue-light" style={{ backgroundImage: `url(${profileBg})` }}>
+      {/* Bagian Ornamen Atas*/}
+      <img
+        src={flower2}
+        alt="Ornamen Bunga Atas Tengah"
+        className="absolute top-[-65px] left-1/2 -translate-x-1/2 w-[150px] max-w-md h-auto z-10 transform rotate-90"
+      />
+      <img
+        src={flower}
+        alt="Ornamen Bunga Atas Kiri"
+        className="absolute top-0 left-[-10px] w-[300px] max-w-md h-auto z-10 rotate-180"
+      />
+      <img
+        src={flower}
+        alt="Ornamen Bunga Atas Kanan"
+        className="absolute top-0 right-[-10px] w-[300px] max-w-md h-auto z-10 transform scale-y-[-1]"
+      />
+      <img
+        src={bismillah}
+        alt="Bismillah"
+        className="absolute top-[140px] left-1/2 -translate-x-1/2 w-[270px] max-w-md h-auto z-10"
+      />
+
+      <p className="max-w-3xl mx-auto mb-12 mt-[150px]">
         Maha suci Allah yang telah menciptakan makhluk-Nya berpasang-pasangan. Dengan memohon rahmat dan ridho Allah Subhanahu Wa Ta'ala, kami bermaksud menyelenggarakan pernikahan putra-putri kami:
       </p>
       <div className="flex flex-col md:flex-row justify-center items-center gap-8 md:gap-12">
@@ -36,6 +64,28 @@ const Profile = () => {
           isBride={false}
         />
       </div>
+
+      {/* Bagian Ornamen Bawah*/}
+      <img
+        src={flower3}
+        alt="Ornamen Bunga Bawah Kiri"
+        className="absolute bottom-[35rem] left-[-80px] w-[200px] max-w-md h-auto z-10 opacity-80 rotate-[35deg]"
+      />
+      <img
+        src={flower3}
+        alt="Ornamen Bunga Bawah Kanan"
+        className="absolute bottom-[25rem] right-[-80px] w-[200px] max-w-md h-auto z-10 opacity-80 rotate-[-35deg]"
+      />
+      <img
+        src={fern}
+        alt="Ornamen Pakis Bawah Kiri"
+        className="absolute bottom-0 left-0 w-[350px] max-w-md h-auto z-0 opacity-80"
+      />
+      <img
+        src={fern2}
+        alt="Ornamen Pakis Bawah Kiri"
+        className="absolute bottom-[30px] right-[-45px] w-[320px] max-w-md h-auto z-0 opacity-80 rotate-90 "
+      />
     </div>
   );
 };
