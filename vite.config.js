fe75e5e67fabce579a279ac42eba5e7a0a3bed14
kellyman1717 <akuntumbal9419@gmail.com>
2025-12-07 14:35:15 +0700

    membalikkan versi vite.config.js ke semula

--- vite.config.js
+++ vite.config.js
@@ -4,25 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000, 
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('leaflet') || id.includes('react-leaflet')) {
-              return 'maps-vendor';
-            }
-            if (id.includes('firebase')) {
-              return 'firebase-vendor';
-            }
-            if (id.includes('framer-motion')) {
-              return 'motion-vendor';
-            }
-            return 'vendor'; 
-          }
-        },
-      },
-    },
-  },
 })
